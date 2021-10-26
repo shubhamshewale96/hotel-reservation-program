06f230276c59d86b3684060483176098b628743d
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-26 08:36:59 +0530

    welcome to hotel reservation program

---
+++ lib/src/main/java/hotel/reservation/HotelReservationMain.java
@@ -0,0 +1,10 @@
+package hotel.reservation;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("welcome to Hotel Reservation program");
+	}
+
+}
